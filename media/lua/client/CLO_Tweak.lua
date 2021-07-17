d425a734d73f585f498b192a3afbae2d41c29dbd
MafiaCartel <mafiacartelapp@gmail.com>
2021-07-17 13:37:54 -0400

    Added missing and new items to the tweak list.

--- media/lua/client/CLO_Tweak.lua
+++ media/lua/client/CLO_Tweak.lua
@@ -1,9 +1,15 @@
 local liquidContainerCategory = "Coco_Liquid_Container"
 
 local items = {
+	EmptyPetrolCan = {"Base.EmptyPetrolCan", liquidContainerCategory},
+	PetrolCan = {"Base.PetrolCan", liquidContainerCategory},
+	PetrolCanWater = {"Base.PetrolCanWater", liquidContainerCategory},
 	Coco_WaterGallonEmpty = {"CocoLiquidOverhaulItems.Coco_WaterGallonEmpty", liquidContainerCategory},
 	Coco_WaterGallonFull = {"CocoLiquidOverhaulItems.Coco_WaterGallonFull", liquidContainerCategory},
-	Coco_WaterGallonPetrol = {"CocoLiquidOverhaulItems.Coco_WaterGallonPetrol", liquidContainerCategory}
+	Coco_WaterGallonPetrol = {"CocoLiquidOverhaulItems.Coco_WaterGallonPetrol", liquidContainerCategory},
+	Coco_LargeEmptyPetrolCan = {"CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan", liquidContainerCategory},
+	Coco_LargePetrolCan = {"CocoLiquidOverhaulItems.Coco_LargePetrolCan", liquidContainerCategory},
+	Coco_LargePetrolCanWater = {"CocoLiquidOverhaulItems.Coco_LargePetrolCanWater", liquidContainerCategory},
 }
 
 function CLO_TweakItems()
