38d63222ffbcc9062e52f7c8c49abf6e21586ba9
MafiaCartel <mafiacartelapp@gmail.com>
2021-07-19 20:44:05 -0400

    Calling override methods OnLoad and moved the item category tweak to the main file.

--- media/lua/client/main/CLO_Main.lua
+++ media/lua/client/main/CLO_Main.lua
@@ -2,6 +2,14 @@
 local function OnLoad()
     print(CLO_ModSettings.Name .. " v" .. tostring(CLO_ModSettings.Version) .. " has loaded!")
 
+    ---Do overrides
+    CLO_Override_ISTakeFuel()
+    CLO_Override_ISAddGasolineToVehicle()
+    CLO_Override_ISTakeGasolineFromVehicle()
+    CLO_Override_ISVehicleMenu_FillPartMenu()
+    CLO_Override_ISVehiclePartMenu_getGasCanNotEmpty()
+    CLO_Override_ISVehiclePartMenu_getGasCanNotFull()
+
     for i = 1, #CLO_ModSettings.PreloadLogs do
         print(CLO_ModSettings.PreloadLogs[i])
     end
@@ -9,8 +17,22 @@ local function OnLoad()
     CLO_ModSettings.Loaded = true
 end
 
+---OnGameBoot
+local function OnGameBoot()
+    for _, v in pairs(CLO_ModSettings.CustomItems) do
+        local item = ScriptManager.instance:getItem(v[1])
+        if item ~= nil then
+            item:DoParam("displaycategory" .. " = " .. v[2])
+        end
+    end
+end
+
+---Add new GasCan like items
+CLO_AddNewFuelItem("CocoLiquidOverhaulItems", "Coco_LargeEmptyPetrolCan", "Coco_LargePetrolCan")
+CLO_AddNewFuelItem("CocoLiquidOverhaulItems", "Coco_WaterGallonEmpty", "Coco_WaterGallonPetrol")
+
 ---Game Events
-Events.OnGameBoot.Add(CLO_TweakItems)
+Events.OnGameBoot.Add(OnGameBoot)
 Events.OnLoad.Add(OnLoad)
 Events.OnPreFillInventoryObjectContextMenu.Add(CLO_Contexts.Context_CheckDrainableContent)
 Events.OnPreFillInventoryObjectContextMenu.Add(CLO_Contexts.Context_PourGasInto)
