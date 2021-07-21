f8fc69713b5f2ab1b24375ac107eb1d6cacfe011
MafiaCartel <mafiacartelapp@gmail.com>
2021-07-21 14:28:20 -0400

    Fixed Distributions for new pz update.

--- media/lua/server/Items/CLO_Server_Distribution_LargeGasCan.lua
+++ media/lua/server/Items/CLO_Server_Distribution_LargeGasCan.lua
@@ -1,4 +1,5 @@
 require "Items/SuburbsDistributions"
+require "Items/ProceduralDistributions"
 
 -- ITEMS
 --- CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan
@@ -9,12 +10,21 @@ require "Items/SuburbsDistributions"
 --- shed
 --- garagestorage
 --- garage
---- mechanic
 --- storageunit
 --- gasstore
 --- gasstorage
 --- armyhanger
 --- armyhanger
+
+--- ProceduralDistributions
+
+-- StoreShelfMechanics
+table.insert(ProceduralDistributions.list.StoreShelfMechanics.items, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
+table.insert(ProceduralDistributions.list.StoreShelfMechanics.items, 5)
+table.insert(ProceduralDistributions.list.StoreShelfMechanics.junk, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
+table.insert(ProceduralDistributions.list.StoreShelfMechanics.junk, 5)
+
+--- SuburbsDistributions
 
 -- all
 table.insert(SuburbsDistributions["all"]["bin"].items, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
@@ -38,29 +48,11 @@ table.insert(SuburbsDistributions["garage"]["metal_shelves"].items, 2)
 table.insert(SuburbsDistributions["garage"]["metal_shelves"].items, "CocoLiquidOverhaulItems.Coco_LargePetrolCan")
 table.insert(SuburbsDistributions["garage"]["metal_shelves"].items, 1.5)
 
--- mechanic
-table.insert(SuburbsDistributions["mechanic"]["metal_shelves"].items, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
-table.insert(SuburbsDistributions["mechanic"]["metal_shelves"].items, 1.5)
-table.insert(SuburbsDistributions["mechanic"]["metal_shelves"].items, "CocoLiquidOverhaulItems.Coco_LargePetrolCan")
-table.insert(SuburbsDistributions["mechanic"]["metal_shelves"].items, 0.5)
-
 -- storageunit
 table.insert(SuburbsDistributions["storageunit"]["all"].items, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
 table.insert(SuburbsDistributions["storageunit"]["all"].items, 2)
 table.insert(SuburbsDistributions["storageunit"]["all"].items, "CocoLiquidOverhaulItems.Coco_LargePetrolCan")
 table.insert(SuburbsDistributions["storageunit"]["all"].items, 1.5)
-
--- gasstore
-table.insert(SuburbsDistributions["gasstore"]["shelves"].items, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
-table.insert(SuburbsDistributions["gasstore"]["shelves"].items, 7.5)
-table.insert(SuburbsDistributions["gasstore"]["shelves"].items, "CocoLiquidOverhaulItems.Coco_LargePetrolCan")
-table.insert(SuburbsDistributions["gasstore"]["shelves"].items, 5)
-table.insert(SuburbsDistributions["gasstore"]["counter"].items, "CocoLiquidOverhaulItems.Coco_LargeEmptyPetrolCan")
-table.insert(SuburbsDistributions["gasstore"]["counter"].items, 1)
-
--- gasstorage
-table.insert(SuburbsDistributions["gasstorage"]["metal_shelves"].items, "CocoLiquidOverhaulItems.Coco_LargePetrolCan")
-table.insert(SuburbsDistributions["gasstorage"]["metal_shelves"].items, 3)
 
 -- armyhanger
 table.insert(SuburbsDistributions["armyhanger"]["metal_shelves"].items, "CocoLiquidOverhaulItems.Coco_LargePetrolCan")
